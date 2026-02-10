822447772312ab6bb2a5a74e39d298db4b8bffeb
CarlosFedericoZalazar <carlos-zalazar@hotmail.com>
2026-02-10 17:55:06 -0300

    corrección en endpoint post

--- js/api.js
+++ js/api.js
@@ -7,7 +7,7 @@ export async function getTasks() {
 }
 
 export async function createTask(texto) {
-    const res = await fetch(`${API_URL}/tasks/post/`, {
+    const res = await fetch(`${API_URL}/tasks/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ texto })
